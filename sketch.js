3505a9c025d555522bb1911b6cbefd499da216a5
shivanshu <shivanshu>
2019-09-23 19:06:32 +0530

    constraintbodies

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
@@ -35,7 +36,8 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
-
+    log6 = new Log(230,180,80,PI/2);
+    chain = new Chain(bird.body,log6.body);
 }
 
 function draw(){
@@ -58,7 +60,9 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+    log6.display();
 
     bird.display();
+    chain.display();
     platform.display();
 }
